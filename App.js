135638236adf0f011b1d0e5069d20b1d1eb3fb59
LEONARDO MOCKAITES <leonardo.mockaites@senacsp.edu.br>
2022-06-07 12:04:43 -0300

    atualizaçao de codigo

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
+import React, {useState, useEffect} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+//import AppLoading from 'expo-app-loading';
+//import {useFonts, Abel_400Regular,Mirza_400Regular,Nunito_400Regular} from '@expo-google-fonts/dev';
+
 
 export default function App() {
+  
+  //let[fontsLoaded] = useFonts({
+    //Abel_400Regular,
+    //Mirza_400Regular,
+    //Nunito_400Regular
+  //});
+ 
+  //if (!fontsLoaded) {
+    //return <AppLoading />;
+  //} else {
+
   return (
-    <View style={styles.container}>
-      <Text>Redes de Encontro do Senac</Text>
-      <Text>Você tem um projeto integrador?</Text>
-      <Text>Quer envolver novas áreas?</Text>
-      <Text>Então vamos encontrar novos alunos aqui. ;)</Text>
+    <View style={estilo.container}>
+      <StatusBar style="auto"/>
+      <Text style={estilo.titulo1}>Redes de Encontro do Senac</Text>
+      <Text style={estilo.titulo2}>Você tem um projeto integrador?</Text>
+      <Text style={estilo.titulo3}>Quer envolver novas áreas?</Text>
+      <Text style={estilo.titulo4}>Então vamos encontrar novos alunos aqui. ;)</Text>
 
       <FlatList
       data={pessoas}
@@ -19,6 +35,7 @@ export default function App() {
         <Text>{item.Turma}</Text>
         <Text>{item.Gosto}</Text>
         <Text>{item.Bio}</Text>
+        <Image style={estilo.img} source={item.Img}/>
       </View>
       }
       />
@@ -26,15 +43,45 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#DAA520',
+    backgroundColor: '#4169E1',
     alignItems: 'center',
     justifyContent: 'center',
-    
-    
   },
+
+  img:{
+    width: 250,
+    height: 200,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+
+  titulo1:{
+    marginTop: 40,
+    marginVertical:5,
+    fontSize: 30,
+    fontFamily: 'Abel_400Regular'
+  },
+
+  titulo2:{
+    marginVertical:2,
+    fontSize: 25,
+    fontFamily:'Mirza_400Regular'
+  },
+
+  titulo3:{
+    marginVertical:2,
+    fontSize:20,
+    fontFamily:'Nunito_400Regular',
+  },
+  titulo4:{
+    marginVertical:0,
+    fontSize:20,
+  }
+
 });
 
 const pessoas =[
@@ -42,11 +89,11 @@ const pessoas =[
     uid:1,
     Nome:'Danilo Soares',
     Curso:'Técnico Informatica',
-    Perido:'Manhã',
+    Periodo:'Manhã',
     Turma:'TI10M',
     Gosto:'Hardware,Front-end,Back-and',
     Bio:'Manuntenção não é de graça',
-    Img:'',
+    Img:require('./assets/jorginho.jpg')
   },
 
   {
@@ -57,18 +104,18 @@ const pessoas =[
     Turma:'TI10M',
     Gosto:'Hardware, Redes',
     Bio:'mais novo membro da conferencia mundial de pods no banheiro ',
-    Img:'',
+    Img:require('./assets/carminha.jpg')
   },
 
   {
     uid:3,
     Nome:'Leonardo Mock',
     Curso:'Técnico Informatica',
-    perido:'Manhã',
+    Periodo:'Manhã',
     Turma:'Ti08M',
     Gosto:'Hardware,Front-end,Back-and',
     Bio:'Manuntenção não é de graça fica esperto',
-    Img:'',
+    Img:require('./assets/tufao.jpg')
   },
 
   {
@@ -79,7 +126,7 @@ const pessoas =[
     Turma:'ti12',
     Gosto:'sofrer',
     Bio:'matar um certo alguem é meu sonho',
-    Img:'',
+    Img:require('./assets/leleco.jpg')
   },
 
   {
@@ -90,18 +137,18 @@ const pessoas =[
     Turma: 'TI10M',
     Gosto: 'Hardware, Back-End, Redes, C#',
     Bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy.',
-    Img:'',
+    Img:require('./assets/nina.jpg')
   },
 
   {
     uid:6,
-    name:'Daniel Lacerda',
+    Nome:'Daniel Lacerda',
     Curso:'Técnico de Informática',
     Periodo:'Manhã',
     Turma:'TI10MM',
     Gosto:'Php, Css, Design Gráfico, Games',
     Bio:'Gosto de Jogar, Gosto de mexer na internet, sou professor de informática, e Gosto de tocar instrumentos como teclado, violão, guitarra e bateria',
-    Img:'',
+    Img:require('./assets/nilo.webp')
   },
 
   {
@@ -112,7 +159,7 @@ const pessoas =[
     Turma:'ti19',
     Gosto:'subir parede',
     Bio:'dormir acordar trabalhar banho e comer e dps tudo de novo',
-    Img:'',
+    Img:require('./assets/cardinho.jpg')
   },
 
   {
@@ -123,7 +170,7 @@ const pessoas =[
     Turma:'TI10M',
     Gosto:'Programação',
     Bio: 'Engajando na empresa da Microsolft',
-    Img:'',
+    Img:require('./assets/Freezar.jpg')
   },
 
   {
@@ -131,9 +178,10 @@ const pessoas =[
     Nome:'Silvio Santos',
     Curso:'Gestão de negócios',
     Periodo: 'Manhã',
+    Turma: 'TI10M',
     Gosto:'Apresentador,Administração',
     Bio:'Curto a vida de um geito bem leve, Gosto de sair com a minha familia aos finais de semana, sou comunicador e Gosto de ser apresentador de TV esse é o meu forte',
-    Img:'',
+    Img:require('./assets/Freezar.jpg')
   },
 
   {
@@ -144,7 +192,7 @@ const pessoas =[
     Turma: 'TI10M',
     Gosto:'Livia, hosana, laura, Mol e casadas', 
     Bio:'Hardware,,REDES,fazendo programa nois é só o basico, preto lindo, famoso e rico',
-    Img:'',
+    Img:require('./assets/Freezar.jpg')
   },
 
   {
@@ -155,7 +203,7 @@ const pessoas =[
     Turma:'TI10MM',
     Gosto:'Hardware,Banco de dados,C#',
     Bio:'garoto de programa junior',
-    Img:'',
+    Img:require('./assets/Freezar.jpg')
   },
 
   {
@@ -166,7 +214,7 @@ const pessoas =[
     Turma: 'EB10',
     Gosto: 'Bolo, Chantilly e Fuzil',
     Bio: 'Presidente do Brasil, apaixonado por confeitaria. Adoro uma mulher que saiba fazer um bolinho do bom e um mousse cremoso! Geralmente me chamam de diabo, corrupto, maníaco e genocida. Mas eu acho que são apelidos carinhosos do meu povo :)',
-    Img:'',
+    Img:require('./assets/Freezar.jpg')
   },
 
   {
@@ -177,8 +225,8 @@ const pessoas =[
     Turma:'TI10MM',
     Gosto:'Php, Css, Design Gráfico, Games',
     Bio:'Gosto de Jogar, Gosto de mexer na internet, sou professor de informática, e Gosto de tocar instrumentos como teclado, violão, guitarra e bateria',
-    Img:'',
-  },
+    Img:require('./assets/Freezar.jpg')
+  }
 
   
 ];
